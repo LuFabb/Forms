90ae58cf82c6b10b9a48b1eb05bca1c7dd3e41d1
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-06 08:49:50 -0500

    selection tag

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [value, setValue] = useState({ normal: '', text: ''})
+  const [value, setValue] = useState({ normal: '', text: '', select: '' })
   const handleChange = (e) => {
     setValue((state) => ({
       ...state,
@@ -14,6 +14,14 @@ const App = () => {
       {value.length < 5 ? <span>minimum length of 5</span> : null}
       <input type="text" name="normal" value={value.normal} onChange={handleChange} />
       <textarea name='text' onChange={handleChange} value={value.text} />
+
+      <select value={value.select} name="select" onChange={handleChange}>
+        <option value="">-- Selection --</option>
+        <option value="chanchofeliz">Chancho feliz</option>
+        <option value="chanchitofeliz">Chanchito feliz</option>
+        <option value="chanchitotriste">Chanchito triste</option>
+        <option value="felipe">Felipe</option>
+      </select>
     </div>
   )
 }
