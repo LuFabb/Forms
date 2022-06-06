b743bda8df3ecd52a4ec3168f79f50dad1236686
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-05 19:51:07 -0500

    intro to controlled components, the recommended way

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-function App() {
-  const submit = (e) => {
-    e.preventDefault()
-    const data = Array.from(new FormData(e.target))
-    console.log(Object.fromEntries(data))
+import { useState } from 'react'
+
+const App = () => {
+  const [value, setValue] = useState('')
+  const handleChange = (e) => {
+    setValue(e.target.value)
   }
-  return (
-    <form onSubmit={submit}>
-      <div>
-        <span>
-          lala
-        </span>
-        <input name='campo' />
-      </div>
-        <input name="campo-2" />
-        <input type="file" name="archivo"/>
-        <input type="submit" value="Enviar"/>
-    </form>
-    )
+  console.log(value)
+  return(
+    <div>
+      {value.length < 5 ? <span>minimum length of 5</span> : null}
+      <input type="text" name="normal" value={value} onChange={handleChange} />
+    </div>
+  )
 }
+
 
 export default App
