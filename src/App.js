ff4cda86eb7d544d396cc2854767b2819fed2125
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-05 19:32:11 -0500

    archive in uncontrolled components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import { useRef } from 'react'
-
-const App = () => {
-  const input = useRef()
-  const file = useRef()
-  const submit = () => {
-    console.log(input.current.value)
-    console.log(file.current.files[0])
-    const form = new FormData()
-    form.append('archivo', file.current.files[0])
-    form.append('campo', input.current.value)
-    fetch('/lala', { method: 'POST', body: form })
+function App() {
+  const submit = (e) => {
+    e.preventDefault()
+    const data = Array.from(new FormData(e.target))
+    console.log(Object.fromEntries(data))
   }
-
   return (
-    <div>
+    <form onSubmit={submit}>
       <div>
-        <span>Lala</span>
-        <input type="text" name="campo" ref={input} />
-        <input type="file" ref={file} />
+        <span>
+          lala
+        </span>
+        <input name='campo' />
       </div>
-      <input type="submit" value="Enviar" onClick={submit} />
-    </div>
-  )
+        <input name="campo-2" />
+        <input type="file" name="archivo"/>
+        <input type="submit" value="Enviar"/>
+    </form>
+    )
 }
 
 export default App
